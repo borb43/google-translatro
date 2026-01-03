7e498785127f815feafa5c0e25bfb24dbbc4dc15
Eris (borb) <remcosveen@gmail.com>
2026-01-03 13:05:20 +0100

    translate shop sign

--- main.lua
+++ main.lua
@@ -2,3 +2,14 @@ SMODS.Language {
     key = "bad_translate",
     label = "Google Translated"
 }
+
+SMODS.Atlas {
+    key = "shop_sign",
+    path = "market.png",
+    px = 113,
+    py = 57,
+    atlas_table = 'ANIMATION_ATLAS',
+    raw_key = true,
+    frames = 4,
+    prefix_config = { key = false }
+}
